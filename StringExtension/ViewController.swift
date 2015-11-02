c4d862d4e9986930106832a15b575642ff5cb768
sheepy1 <sheepyyoung@foxmail.com>
2015-11-02 22:23:23 +0800

    Initial Commit

---
+++ StringExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringExtension
+//
+//  Created by Sheepy on 15/11/2.
+//  Copyright © 2015年 Sheepy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
